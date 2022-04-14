85785177ad36616e798541a9f28faa53a7f80f88
Elms <jeff@wolfssl.com>
2022-04-14 14:34:42 -0700

    wolfmqtt update

--- ports/wolfmqtt/portfile.cmake
+++ ports/wolfmqtt/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO elms/wolfmqtt
     REF vcpkg
-    SHA512 06daa27f0c814982a73137a83885a9b5c0701f0b7be36219c3da8b6f7aa3ab9fb61bfb938ada685e6ef43b53fefce1e9653c86857153f340889b7e2be11d6a2e
+    SHA512 7a904f85b22dce472f53991ec3441cd6640d2c7295dc736607f3c6ea5aacd7998572702760a2ffdb15492d2b05c97c857b9172f87e06b0c8c3249c21d3cf515b
     HEAD_REF master
     )
 
@@ -11,7 +11,7 @@ vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH} OPTIONS -DWOLFMQTT_EXAMPLES=no)
 vcpkg_cmake_install()
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/wolfmqtt" RENAME copyright)
 
-#vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/wolfmqtt)
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/wolfmqtt)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
