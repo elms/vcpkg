523783a3903c2303e362f33afb942f00c219e38c
Elms <jeff@wolfssl.com>
2022-04-14 14:34:04 -0700

    wolftpm update

--- ports/wolftpm/portfile.cmake
+++ ports/wolftpm/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO elms/wolftpm
     REF vcpkg
-    SHA512  2d22f2d0d533661deba9c1e02d2df6fd7d99045e985c38f74c5a430cbdd0dab761e79cd135159f6adebe05b361eb0cebfbac3c15af36f0754e2e2966daa92f8b
+    SHA512 dd00b5131102caedeed44901fe70c58bd0601c5f60e7c4725e6c59821f2679ce8e8ac5c937f2f087208c8dad7a5da447cece5e9d5806da63e7664b6381194b50
     HEAD_REF master
     )
 
@@ -11,7 +11,6 @@ vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH} OPTIONS -DWOLFTPM_EXAMPLES=no)
 vcpkg_cmake_install()
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/wolftpm" RENAME copyright)
 
-#vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/wolftpm)
-
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/wolftpm)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
